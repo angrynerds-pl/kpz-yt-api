6e7fdd6978dbea266eea81ac45cc75208acae726
Maja Bojarska <majabojarska98@gmail.com>
2020-03-14 19:14:53 +0100

    Implement users module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,10 +3,12 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { PlaylistModule } from '../playlist/playlist.module';
 
 @Module({
-  imports:[TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User]), PlaylistModule],
   providers: [UserService],
-  controllers: [UserController]
+  controllers: [UserController],
+  exports: [UserService],
 })
 export class UserModule {}
