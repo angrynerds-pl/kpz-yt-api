d810ad33099b6419d54e53a766eea692d4221a6a
Wiktor Pieklik <wiktor.pieklik@icloud.com>
2020-03-18 18:37:39 +0100

    Adding imports to module

--- src/playlistitem/playlistitem.module.ts
+++ src/playlistitem/playlistitem.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PlaylistItemController } from './playlistitem.controller';
 import { PlaylistItemService } from './playlistitem.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PlaylistItem } from './entities/playlistitem.entity';
 
 @Module({
   controllers: [PlaylistItemController],
   providers: [PlaylistItemService],
-  exports: [PlaylistItemService]
+  exports: [PlaylistItemService],
+  imports: [TypeOrmModule.forFeature([PlaylistItem])]
 })
 export class PlaylistItemModule {}
