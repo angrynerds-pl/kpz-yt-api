1f21adc40dba45f6d63a06292dc6a75fe3f0f058
Maja Bojarska <majabojarska98@gmail.com>
2020-03-18 21:35:01 +0100

    Add class validators to DTO fields.

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,15 +1,25 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsAlpha, IsAscii, IsAlphanumeric, IsNotEmpty } from 'class-validator';
 
 export class CreateUserDto {
   @ApiProperty({ example: 'demouser' })
+  @IsString()
+  @IsAlphanumeric()
   readonly username: string;
 
   @ApiProperty({ example: 'topsecret' })
+  @IsString()
+  @IsAscii()
+  @IsNotEmpty()
   password: string;
 
   @ApiProperty({ example: 'John' })
+  @IsString()
+  @IsAlpha()
   readonly firstname: string;
 
   @ApiProperty({ example: 'Smith' })
+  @IsString()
+  @IsAlpha()
   readonly surname: string;
 }
--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,16 +1,25 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsOptional } from 'class-validator';
+import { IsOptional, IsString, IsAlpha, IsAscii, IsNotEmpty } from 'class-validator';
 
 export class UpdateUserDto {
   @ApiProperty({ example: 'topsecret', required: false })
   @IsOptional()
+  @IsString()
+  @IsAscii()
+  @IsNotEmpty()
   password?: string;
 
   @ApiProperty({ example: 'John', required: false })
   @IsOptional()
+  @IsString()
+  @IsAlpha()
+  @IsNotEmpty()
   readonly firstname?: string;
 
   @ApiProperty({ example: 'Smith', required: false })
   @IsOptional()
+  @IsString()
+  @IsAlpha()
+  @IsNotEmpty()
   readonly surname?: string;
 }
