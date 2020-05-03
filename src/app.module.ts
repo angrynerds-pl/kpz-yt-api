a48847fe4f525f4ebc9c0503913a3c00e7b7956b
Maja Bojarska <majabojarska98@gmail.com>
2020-05-03 20:20:47 +0200

    Adapt existing modules and services for e2e test running.

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,7 +11,7 @@ import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRootAsync({
+     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       useExisting: ConfigService,
     }),
