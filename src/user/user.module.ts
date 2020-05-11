ec8616e099026b238f87764f4e63041f19f36d20
Maja Bojarska <majabojarska98@gmail.com>
2020-05-11 19:58:18 +0200

    Add top user titles endpoint.

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,22 +1,24 @@
-import { Module, forwardRef } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { PassportModule } from '@nestjs/passport';
 import { ConfigModule } from '../config/config.module';
-import { PlaylistModule } from '../playlist/playlist.module';
 import { PlaylistService } from '../playlist/playlist.service';
 import { Playlist } from '../playlist/entities/playlist.entity';
+import { PlaylistItem } from '../playlist-item/entities/playlist-item.entity';
+import { PlaylistItemService } from '../playlist-item/playlist-item.service';
 
 @Module({
   imports: [
     PassportModule.register({ defaultStrategy: 'jwt' }),
     TypeOrmModule.forFeature([User]),
     TypeOrmModule.forFeature([Playlist]),
+    TypeOrmModule.forFeature([PlaylistItem]),
     ConfigModule,
   ],
-  providers: [UserService, PlaylistService],
+  providers: [UserService, PlaylistService, PlaylistItemService],
   controllers: [UserController],
   exports: [UserService],
 })
